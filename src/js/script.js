f8fb60a1aad90323850079ff26760f4ea85f92ed
tami870 <syu.jin.taka@icloud.com>
2025-08-24 17:36:50 +0900

    20250824_ピクパ完了_追加

--- src/js/script.js
+++ src/js/script.js
@@ -11,35 +11,6 @@ jQuery(function ($) {
     $(".js-drawer").fadeOut();
   });
 
-  // スクロールでヘッダーにクラスを付ける
-  // $(window).on("scroll", function () {
-  //   const scrollTop = $(this).scrollTop();
-  //   const triggerHeight = $(".first-view").outerHeight();
-
-  //   if (scrollTop > triggerHeight) {
-  //     $(".header").addClass("header--scrolled");
-  //   } else {
-  //     $(".header").removeClass("header--scrolled");
-  //   }
-  // });
-
-  // mainコンテンツをヘッダーの高さ分だけ下げる
-  // const headerHeight = $(".js-header").outerHeight();
-  // $("main").css("margin-top", headerHeight);
-
-  // // ページ内スクロール処理（リンククリック）
-  // $('a[href^="#"]').on("click", function (e) {
-  //   const speed = 600;
-  //   const href = $(this).attr("href");
-  //   const target = $(href === "#" || href === "" ? "html" : href);
-
-  //   if (target.length) {
-  //     const position = target.offset().top - headerHeight;
-  //     $("html, body").animate({ scrollTop: position }, speed, "swing");
-  //   }
-  //   e.preventDefault();
-  // });
-
   // ページトップボタンの表示・非表示
   const pageTop = $(".js-page-top");
   pageTop.hide();
